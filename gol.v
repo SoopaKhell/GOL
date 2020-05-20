c2df319e822b65760c45d8d6ff916f5a1ce16ddd
SoopaKhell <carteraprince@gmail.com>
2020-05-20 00:19:05 -0400

    add le code

---
+++ gol.v
@@ -0,0 +1,120 @@
+import os
+
+// makes a deep clone of a 2d array
+fn deep_clone(arr [][]int) [][]int {
+    return arr.map(it.clone())
+}
+
+// prints all the elements in a 2d array of ints
+fn draw_board(board [][]int) {
+    for i := 0; i < board.len; i++ {
+	for j := 0; j < board[0].len; j++ {
+	    if board[i][j] == 1 {
+		print('██')
+	    } else {
+		print('░░')
+	    }
+	}
+	println('')
+    }
+}
+
+// counts the number of live neighbors for a particular coordinate
+fn count_live_neighbors(board [][]int, x_pos int, y_pos int) int {
+    // start in top left of box, stop in bottom right
+    
+    mut live_neighbors := 0
+    
+    for y := -1; y <= 1; y++ {	
+	for x := -1; x <= 1; x++ {
+	    // make sure we don't count the cell itself as a neighbor
+	    if x != 0 || y != 0 {
+		// make sure we don't check cells out of bounds	
+		if y_pos+y >= 0 && board.len > y_pos+y && x_pos+x >= 0 && board[0].len > x_pos+x {
+		    if board[y_pos+y][x_pos+x] == 1 {
+			live_neighbors++
+		    }
+		}
+	    }
+	}
+    }
+    return live_neighbors
+}
+
+// Any live cell with fewer than two live neighbours dies.
+// Any live cell with more than three live neighbours dies.
+// Any dead cell with exactly three live neighbours becomes a live cell.
+
+// calculates the next frame based on the last frame
+fn next_frame(board [][]int) [][]int {
+    mut new_board := deep_clone(board)
+
+    mut live_neighbors := 0
+
+    for y := 0; y < board.len; y++ {
+        for x := 0; x < board[0].len; x++ {
+            live_neighbors = count_live_neighbors(board, x, y)
+
+	    if board[y][x] == 1 { // if the cell is alive
+		// and has less than 2 or more than 3 live neighbors
+		if live_neighbors < 2 || live_neighbors > 3 {
+		    // kill the cell in the next generation
+		    new_board[y][x] = 0
+		}
+	    } else { // if the cell is dead
+		// and has 3 live neighbors
+		if live_neighbors == 3 {
+		    // make a live cell in the next generation
+		    new_board[y][x] = 1
+		}
+	    } 
+        }
+    }
+
+    return new_board
+}
+
+fn main() { 
+    
+    mut board := [
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+    
+    mut generation := 1
+
+    for true {	
+	draw_board(board)
+	board = next_frame(board)
+	os.input("Generation: $generation\nPress enter for next frame... (Ctrl-C to exit)")
+	generation++
+	os.flush()
+    }
+}
